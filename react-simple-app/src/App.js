96bf86fd3bcaf990a93e55a098008dca7cff740b
musto <musti.sar4@gmail.com>
2023-03-30 21:31:45 +0200

    Add a new app

--- react-simple-app/src/App.js
+++ react-simple-app/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload (updated).
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>All Blogs</h1>
       </header>
+      <body>
+        {blogs && blogs.map((blog) => <div key={blog.id}>{blog.title}</div>)}
+      </body>
     </div>
   );
 }
